485c5ceb5bea45e215a80115333091d78d75bd28
scriptrdotio <git@apstrata.com>
2016-06-09 17:00:16 +0300

    Update config.js

--- scripts/predix/oauth2/config.js
+++ scripts/predix/oauth2/config.js
@@ -5,13 +5,13 @@ execute=authenticated
   **/ 
  
  // The URL of the default UAA authentiction service
-var uaa = "https://99de7138-bbdb-4c6a-aa1a-0836122bb4a6.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token";
+var uaa = "<uaa end point>";
 
 // The clientId of the predix application
-var clientId = "traffic";
+var clientId = "<uaa client id>";
 
 // The password of the prefix application
-var clientPassword = "traffic";
+var clientPassword = "<uaa client secret>";
 
 
 
