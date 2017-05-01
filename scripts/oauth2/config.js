c80bea28c191017e9a4ea8035fe641ab5d35c4ee
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2017-04-30 23:02:44 -0700

    upated the config.js with correct values

--- scripts/oauth2/config.js
+++ scripts/oauth2/config.js
@@ -2,16 +2,16 @@
  read=nobody 
 write=nobody
 execute=authenticated 
-  **/ 
- 
- // The URL of the default UAA authentiction service
-var uaa = "<uaa end point>";
+  **/
+
+  // The URL of the default UAA authentiction service
+var uaa = "https://890407d7-e617-4d70-985f-01792d693387.predix-uaa.run.aws-usw02-pr.ice.predix.io";
 
 // The clientId of the predix application
-var clientId = "<uaa client id>";
+var clientId = "hackathon";
 
 // The password of the prefix application
-var clientPassword = "<uaa client secret>";
+var clientPassword = "@hackathon";
 
 
 
